b2e75bdb24cd69fcd516c1343a7df92998a7b33d
Rganthong <74427555+Rganthong@users.noreply.github.com>
2023-07-14 11:53:54 +0700

    Update and rename myconfig_template.ts to myconfig.ts

--- examples/src/myconfig.ts
+++ examples/src/myconfig.ts
@@ -1,5 +1,5 @@
-export const appKey = "YOUR_APP_KEY=";
+export const appKey = "LzhU74u5rvWKXtNFcIZ6hY2i7X8weL9N5WUG7wctHiw=";
 export const nodeUrl = "https://sdk.mindnetwork.xyz"; // or change to other node url
-export const aliceWalletAddress = "Alice_Wallet_Address";
+export const aliceWalletAddress = "0xb2013951E3dD62d59E743626D17e771a12DC68b7";
 export const bobWalletAddress = "Bob_Wallet_Address";
 export const charlieWalletAddress = "Charlie_Wallet_Address";
